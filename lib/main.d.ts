82cd948e29043d02cd57f60f9b2f7ac0fa58ef23
saggiyogesh <saggiyogesh@gmail.com>
2020-09-01 19:59:43 +0530

    build

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,4 +1,7 @@
 import * as cluster from './cluster';
 import * as project from './project';
 import * as customApps from './customApps/main';
-export { cluster, project, customApps };
+import * as hpa from './hpa';
+import * as pods from './pods';
+import * as workloads from './workloads';
+export { cluster, project, customApps, hpa, pods, workloads };
