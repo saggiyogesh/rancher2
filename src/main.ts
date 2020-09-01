c9261eeac1bfd46da89c712eb4713b6b5f406b44
saggiyogesh <saggiyogesh@gmail.com>
2020-09-01 19:59:07 +0530

    exports

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import assert from 'assert';
 import * as cluster from './cluster';
 import * as project from './project';
 import * as customApps from './customApps/main';
+import * as hpa from './hpa';
+import * as pods from './pods';
+import * as workloads from './workloads';
 
 const { RANCHER_API_KEY, RANCHER_SERVER_API } = process.env;
 
 assert(RANCHER_API_KEY, 'Missing env var RANCHER_API_KEY');
 assert(RANCHER_SERVER_API, 'Missing env var RANCHER_SERVER_API');
 
-export { cluster, project, customApps };
+export { cluster, project, customApps, hpa, pods, workloads };
 
 process.on('unhandledRejection', (reason, promise) => {
   console.log('Unhandled Rejection at:', promise, 'reason:', reason);
